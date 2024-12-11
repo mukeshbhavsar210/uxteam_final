90992839808317f451c1f1da83082639ff37e2eb
Mukesh <mukeshbhavsar210@gmai.com>
2024-12-11 12:44:56 +0530

    work progress well

--- app/Livewire/Blogs.php
+++ app/Livewire/Blogs.php
@@ -24,6 +24,7 @@ class Blogs extends Component
                 abort(404);
             }
 
+            
             $articles = Article::orderBy('created_at','DESC')
                 ->where('category_id',$category->id)
                 ->where('status',1)
